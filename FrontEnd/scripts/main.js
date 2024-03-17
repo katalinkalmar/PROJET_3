16c5d8b8e4d6aaaa4acc387b3929a3e797e059ec
katalinkalmar <110538626+katalinkalmar@users.noreply.github.com>
2024-03-17 12:22:57 +0100

    Botons dynamiques terminés

--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -18,16 +18,32 @@ console.log(categories)
 const ListeBoutons = document.getElementById("liste-de-boutons");
 
 
+
+
+// Maintenant on s'attaque aux bouton dynamiques.
 // Création du bouton "Tous"
-// On crée la balise input
+// On crée la balise input qui sont les boutons de la liste-de-boutons
 
 const boutonElement = document.createElement("input");
-//On rajoute les attributs id, value, type
+//On rajoute les attributs id, value, type ***
 boutonElement.id = "Tous";
 boutonElement.value = "Tous";
 boutonElement.type = "button";
+
+boutonElement.addEventListener("click", function () { 
+    console.log("Tous")
+  
+    // - On vide la gallery avec InnerHTML (voir cours Mettre à jour l'affichage de la page web OC)
+    gallery.innerHTML = "";
+   
+        //- On affiche, la liste complète
+    Afficher_Liste(works)
+});
+
 // On incorpore le bouton dans la balise liste-des-boutons
 ListeBoutons.appendChild(boutonElement);
+
+
 
 // Création des boutons pour chaque catégorie
 for (let i = 0; i < categories.length; i++) {
@@ -36,79 +52,17 @@ for (let i = 0; i < categories.length; i++) {
     boutonElement.id = categorieActuelle.name;
     boutonElement.value = categorieActuelle.name;
     boutonElement.type = "button";
+
+    boutonElement.addEventListener("click", function () { funcFiltrer(categorieActuelle.id )});
     ListeBoutons.appendChild(boutonElement);
 }
-
-// Je fais un bouton de tout;  Ici viendra le code pour le bouton tout
-// Je relie le btn1 à la fonction à la fonction funcFiltrer1
-var btn1 = document.getElementById("tous");
-btn1.addEventListener("click", funcFiltrer1);
-function funcFiltrer1() {
-    console.log("bouton_tous")
-    // Ici vient le code pour afficher tous les travaux (works)
-    // - On vide la gallery avec InnerHTML (voir cours OC)
-    gallery.innerHTML = "";
-    //- On affiche, la liste complète, à l'aide de la fonction Afficher_Liste, 
-    Afficher_Liste(works)
-}
-
-// On répète les mêmes étapes que bour le btn1
-
-var btn2 = document.getElementById("objets");
-btn2.addEventListener("click", funcFiltrer2);
-function funcFiltrer2() {
-    console.log("bouton_objet")
-    //Ici vient le code pour afficher les objets
-    // - On vide la gallery avec InnerHTML (voir cours Mettre à jour l'affichage de la page web OC)
-    gallery.innerHTML = "";
-    // - On filtre la liste works
-    // Je crée une variable qui est ma liste works filtrée ;          point: je veux ; filter est une boucle cachée
-    //Ceci est l'utilisation de la fonction filter pour trier la liste works
-
-    let works_filtres = works.filter(function (work) { return work.categoryId == 1 })
-    //- On affiche, à l'aide d'une boucle for, la liste filtrée
-    Afficher_Liste(works_filtres)
-}
-
-var btn3 = document.getElementById("appartements");
-btn3.addEventListener("click", funcFiltrer3);
-function funcFiltrer3() {
-    console.log("bouton_objet")
-    //Ici vient le code pour afficher les objets
-    // - On vide la gallery avec InnerHTML (voir cours Mettre à jour l'affichage de la page web OC)
-    gallery.innerHTML = "";
-    // - On filtre la liste works
-    // Je crée une variable qui est ma liste works filtrée ;          point: je veux ; filter est une boucle cachée
-    //Ceci est l'utilisation de la fonction filter pour trier la liste works
-
-    let works_filtres = works.filter(function (work) { return work.categoryId == 2 })
-    //- On affiche, à l'aide d'une boucle for, la liste filtrée
-    Afficher_Liste(works_filtres)
-}
-
-var btn4 = document.getElementById("hotels-et-restaurants");
-btn4.addEventListener("click", funcFiltrer4);
-function funcFiltrer4() {
-    console.log("bouton_objet")
-    //Ici vient le code pour afficher les objets
-    // - On vide la gallery avec InnerHTML (voir cours Mettre à jour l'affichage de la page web OC)
-    gallery.innerHTML = "";
-    // - On filtre la liste works
-    // Je crée une variable qui est ma liste works filtrée ;          point: je veux ; filter est une boucle cachée
-    //Ceci est l'utilisation de la fonction filter pour trier la liste works
-
-    let works_filtres = works.filter(function (work) { return work.categoryId == 3 })
-    //- On affiche, à l'aide d'une boucle for, la liste filtrée
-    Afficher_Liste(works_filtres)
-}
-
-
 
 
 // Ceci est la fonction Afficher_Liste qui sert à afficher tous les éléments d'une liste
 function Afficher_Liste(liste) {
     for (let i = 0; i < liste.length; i++) {
-        // On crée une variable (constante) contenant le premier projet des travaux, works est la liste des works
+        // On crée une letiable  contenant le premier élément des travaux
+
         let work = liste[i];
         //Je crée un élément figure, puis img, puis figcaption
         const figureElement = document.createElement("figure");
@@ -130,7 +84,33 @@ function Afficher_Liste(liste) {
     }
 }
 
-// On appoelle la fonction Afficher_Liste avec comme paramètre works pour afficher la liste complète des travaux lorsque l'utilisateur accède au site 
+// On appelle la fonction Afficher_Liste avec comme paramètre works 
+// pour afficher la liste complète des travaux lorsque l'utilisateur accède au site 
+
 Afficher_Liste(works)
 
 // Par la suite je vais relier les boutons créés de manière dynamique à une fonction qui trie et affiche les travaux
+
+function funcFiltrer(categorie) {
+    console.log(categorie)
+    //Ici vient le code pour afficher les objets
+    // - On vide la gallery avec InnerHTML (voir cours Mettre à jour l'affichage de la page web OC)
+    gallery.innerHTML = "";
+    // - On filtre la liste works
+    // Je crée une variable qui est ma liste works filtrée ;         
+    //Ceci est l'utilisation de la fonction filter pour trier la liste works
+
+    let works_filtres = works.filter(function (work) { return work.categoryId == categorie})
+    //- On affiche, à l'aide d'une boucle for, la liste filtrée
+    Afficher_Liste(works_filtres)
+}
+
+// Je me connecte au serveur 
+
+let reponse_login = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  body: '{"email": "sophie.bluel@test.tld", "password": "S0phie"}'
+});
+
+console.log(reponse_login)
