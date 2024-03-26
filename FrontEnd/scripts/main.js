a0e0bd1223b689250e48e58225192ef3e15e924f
katalinkalmar <110538626+katalinkalmar@users.noreply.github.com>
2024-03-26 20:40:26 +0100

    modal V1

--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -105,4 +105,76 @@ function funcFiltrer(categorie) {
     Afficher_Liste(works_filtres)
 }
 
-console.log(window.localStorage.getItem("token"))
+let modal = null
+
+let previouslyFocusedElement = null
+
+const openModal = async function (e) {
+    e.preventDefault()
+    const target = e.target.getAttribute('href')
+
+    modal = document.querySelector(target)
+
+    previouslyFocusedElement = document.querySelector(':focus')
+    modal.style.display = null
+
+    modal.removeAttribute('aria-hidden')
+    modal.setAttribute('aria-modal', 'true')
+    modal.addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
+
+/* on récupère la div modal-content*/
+    modal_content = document.getElementById("modal-content")
+}
+
+const closeModal = function (e) {
+    if (modal === null) return
+    if (previouslyFocusedElement !== null) previouslyFocusedElement.focus()
+    e.preventDefault()
+
+    modal.setAttribute('aria-hidden', 'true')
+    modal.removeAttribute('aria-modal')
+    modal.removeEventListener('click', closeModal)
+    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
+    const hideModal = function () {
+        modal.style.display = "none"
+        modal.removeEventListener('animationend', hideModal)
+        modal = null
+    }
+    modal.addEventListener('animationend', hideModal)
+}
+
+const stopPropagation = function (e) {
+    e.stopPropagation()
+}
+
+document.querySelectorAll('.js-modal').forEach(a => {
+    a.addEventListener('click', openModal)
+})
+
+function Afficher_Liste_modal(liste) {
+    for (let i = 0; i < liste.length; i++) {
+        // On crée une variable  contenant le premier élément des travaux
+
+        let work = liste[i];
+        //Je crée un élément figure, puis img, puis figcaption
+        const figureElement = document.createElement("figure");
+        const imageElement = document.createElement("img");
+        const nomElement = document.createElement("figcaption");
+
+        //Configuration des différents éléments
+        // Je configure l'attribut src de l'image avec l'URL du projet à afficher
+        imageElement.src = work.imageUrl;
+        imageElement.alt = work.title;
+        // Dans la balise nomElement je veux le innerText et je le définis par, dans mon work,le nom
+        nomElement.innerText = work.title;
+
+        //Dans ma gallery je rajoute un enfant qui est figureElement
+        modal_content.appendChild(figureElement);
+        //On rajoute dans la figure la balise img et figcaption
+        figureElement.appendChild(imageElement);
+        figureElement.appendChild(nomElement);
+    }
+}
