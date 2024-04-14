a3359da6e2d012adfaf01c0da8c90068c4929fd3
katalinkalmar <110538626+katalinkalmar@users.noreply.github.com>
2024-04-14 18:18:19 +0200

    Jstermine

--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -15,11 +15,12 @@ const ListeBoutons = document.getElementById("liste-de-boutons");
 //bouton login/logout
 const loginButton = document.getElementById("login-button");
 
-console.log("chargement de la page")
+
+console.log("chargement de la page") 
+
 
 //on récupère le token de l'utilisateur
-const token = localStorage.getItem("token")
-
+const token = window.localStorage.getItem("token") 
 //on regarde si l'utilisateur est authentifié
 if (token === null) {
     // parcours utilisateur non authentifié
@@ -28,18 +29,23 @@ if (token === null) {
     // parcours utilisateur authentifié   
     loginButton.innerText = "logout"
     loginButton.addEventListener("click", logoutFunction)
+    const bouton_outil = document.querySelector(".js-modal")
+    bouton_outil.style.display = "inline"
 }
 
 function logoutFunction() {
-    localStorage.removeItem("token")
-    window.location.reload()
+    window.localStorage.removeItem("token") 
+    window.location.reload() 
 }
 function loginFunction() { window.location.href = "login_page.html" }
+
+
+
 // Maintenant on s'attaque aux boutons dynamiques.
 // Création du bouton "Tous"
 // On crée la balise input qui sont les boutons de la liste-de-boutons
-
 const boutonElement = document.createElement("input");
+
 //On rajoute les attributs id, value, type 
 boutonElement.id = "Tous";
 boutonElement.value = "Tous";
@@ -48,20 +54,21 @@ boutonElement.type = "button";
 boutonElement.addEventListener("click", function () {
     dernier_bouton_actif.style.backgroundColor = "white";
     dernier_bouton_actif.style.color = "#1D6154";
+    // le bouton cliqué devient le nouveau "dernier bouton actif"
     dernier_bouton_actif = boutonElement;
 
     boutonElement.style.backgroundColor = "#1D6154";
-    boutonElement.style.color = "white"
+    boutonElement.style.color = "white";
 
     //- On affiche, la liste complète
-    Afficher_Liste(works)
+    Afficher_Liste(works);
 });
 
 // On incorpore le bouton dans la balise liste-des-boutons
 ListeBoutons.appendChild(boutonElement);
 let dernier_bouton_actif = boutonElement;
 boutonElement.style.backgroundColor = "#1D6154";
-boutonElement.style.color = "white"
+boutonElement.style.color = "white";
 
 // Création des boutons pour chaque catégorie
 for (let i = 0; i < categories.length; i++) {
@@ -70,6 +77,7 @@ for (let i = 0; i < categories.length; i++) {
     boutonElement.id = categorieActuelle.name;
     boutonElement.value = categorieActuelle.name;
     boutonElement.type = "button";
+  
     boutonElement.addEventListener("click", function () {
         dernier_bouton_actif.style.backgroundColor = "white";
         dernier_bouton_actif.style.color = "#1D6154";
@@ -99,7 +107,7 @@ function Afficher_Liste(liste) {
         const nomElement = document.createElement("figcaption");
 
         //Configuration des différents éléments
-        // Je configure l'attribut src de l'image avec l'URL du projet à afficher
+        // Je configure l'attribut src de l'image avec l'URL du work à afficher
         imageElement.src = work.imageUrl;
         imageElement.alt = work.title;
         // Dans la balise nomElement je veux le innerText et je le définis par, dans mon work,le nom
@@ -116,89 +124,96 @@ function Afficher_Liste(liste) {
 // On appelle la fonction Afficher_Liste avec comme paramètre works,
 // pour afficher la liste complète des travaux lorsque l'utilisateur accède au site 
 
-Afficher_Liste(works)
+Afficher_Liste(works);
 
-// Par la suite je vais relier les boutons créés de manière dynamique à une fonction qui trie et affiche les travaux
+// Par la suite je vais relier les boutons créés de manière dynamique à une fonction qui trie et affiche les travaux par catégorie
 function funcFiltrer(categorie) {
     //Ici vient le code pour afficher les objets
 
     // - On filtre la liste works
     // Je crée une variable qui est ma liste works filtrée ;         
 
-    //Ceci est l'utilisation de la fonction filter pour trier la liste works
-    let works_filtres = works.filter(function (work) { return work.categoryId == categorie })
+    //Ceci est l'utilisation de la fonction filter pour trier la liste works, la fonction va chercher la catégorie demandée.
+    let works_filtres = works.filter(function (work) { return work.categoryId == categorie });
 
     //- On affiche, à l'aide d'une boucle for, la liste filtrée
-    Afficher_Liste(works_filtres)
+    Afficher_Liste(works_filtres);
 }
 
 // ----- code de la modale-----
-let modal = null
 
-let previouslyFocusedElement = null
+let modal = null;
+
+
 
 const openModal = async function (e) {
-    e.preventDefault()
-    const target = e.target.getAttribute('href')
-    console.log("modale")
-    modal = document.querySelector(target)
+    e.preventDefault();
 
-    previouslyFocusedElement = document.querySelector(':focus')
-    modal.style.display = null
+    //dans notre cas target est la valeur de href , càd #modal
+    const target = e.target.getAttribute('href');
+    console.log("modal");
+    modal = document.querySelector(target);
 
-    modal.removeAttribute('aria-hidden')
-    modal.setAttribute('aria-modal', 'true')
+
+    modal.style.display = null;
+
+    modal.removeAttribute('aria-hidden');
+    modal.setAttribute('aria-modal', 'true');
+
     modal.addEventListener('click', closeModal)
-    modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
-    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
 
 
 
-    Afficher_Liste_modal(works)
+    Afficher_Liste_modal(works);
 }
 
 const closeModal = function (e) {
-    if (modal === null) return
-    if (previouslyFocusedElement !== null) previouslyFocusedElement.focus()
-    e.preventDefault()
+    if (modal === null) return;
+   
+    e.preventDefault();
 
-    modal.setAttribute('aria-hidden', 'true')
-    modal.removeAttribute('aria-modal')
-    modal.removeEventListener('click', closeModal)
-    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal)
-    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
+    modal.setAttribute('aria-hidden', 'true');
+    modal.removeAttribute('aria-modal');
+    modal.removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
     const hideModal = function () {
-        modal.style.display = "none"
-        modal.removeEventListener('animationend', hideModal)
-        modal = null
-    }
-    modal.addEventListener('animationend', hideModal)
-}
+        modal.style.display = "none";
+        modal.removeEventListener('animationend', hideModal);
+        modal = null;
+    };
+    modal.addEventListener('animationend', hideModal);
+};
 
-const stopPropagation = function (e) { e.stopPropagation() }
+function stopPropagation(e) { e.stopPropagation(); }
 
-document.querySelector('.js-modal').addEventListener('click', openModal)
+document.querySelector('.js-modal').addEventListener('click', openModal);
 
 
 function Afficher_Liste_modal(liste) {
     /* on récupère la div modal-content*/
     let modal_content = document.getElementById("modal-content");
+
     // - On vide la gallery avec InnerHTML avant d'afficher les nouveaux éléments
     modal_content.innerHTML = "";
+
+    let work = null
     for (let i = 0; i < liste.length; i++) {
-        // On crée une variable  contenant le premier élément des travaux
-        let work = liste[i];
+        // On crée une variable  contenant les travaux
+         work = liste[i];
 
         const button_element = document.createElement("button");
         button_element.classList.add("modal-card");
         // on définit une fonction anonyme pour chacun des boutons
-        button_element.addEventListener("click", function () { delete_work(work.id) })
+        button_element.addEventListener("click", function () { delete_work(work.id) });
 
 
         const img_element = document.createElement("img");
         img_element.classList.add("modal-card-img");
         img_element.src = work.imageUrl;
-        button_element.appendChild(img_element)
+        button_element.appendChild(img_element);
 
 
         const trash_element = document.createElement("img");
@@ -207,36 +222,9 @@ function Afficher_Liste_modal(liste) {
         button_element.appendChild(trash_element);
 
 
-        //Dans ma gallery je rajoute un enfant qui est figureElement
+        //Dans ma gallery je rajoute un enfant qui est button_element
         modal_content.appendChild(button_element);
     }
-}
-
-
-function delete_work(id) {
-    console.log(id)
-
-    /* D'après le swagger on rajoute le token dans le header*/
-    fetch(`http://localhost:5678/api/works/${id}`, {
-        method: "DELETE",
-        headers: { Authorization: `Bearer ${token}` }
-    }).then(function () {
-        //suppression du travail dans le html  
-        // on filtre notre liste works en retirant celui qui a été supprimé sur le serveur
-        works = works.filter(function (work) {
-            return work.id !== id
-        })
-        // on réactualise l'affichage avec la nouvelle liste
-        Afficher_Liste(works)
-        Afficher_Liste_modal(works)
-
-    }).catch(
-        //affichage de l'erreur
-        (error) => {
-            console.log(error)
-
-        }
-    )
 }
 
 //On récupère parmi les formulaires celui qui s'appelle fileinfo
@@ -254,13 +242,13 @@ form.addEventListener("submit", (event) => {
         body: formData
     }).then(function (reponse) {
         //on formate la réponse en format lisible
-        const result = reponse.json()
+        const result = reponse.json();
         result.then(function (work) {
-            console.log(work)
+            console.log(work);
             // on rajoute le travail retourné par le serveur à notre liste de work
-            works.push(work)
-            Afficher_Liste(works)
-            Afficher_Liste_modal(works)
+            works.push(work);
+            Afficher_Liste(works);
+            Afficher_Liste_modal(works);
         })
 
     }
@@ -273,11 +261,38 @@ form.addEventListener("submit", (event) => {
     )
 
 
-},
-    false,
-);
+});
 
-// Remplissage du select pour les catégories
+
+function delete_work(id) {
+    console.log(id);
+
+    /* D'après le swagger on rajoute le token dans le header*/
+    fetch(`http://localhost:5678/api/works/${id}`, {
+        method: "DELETE",
+        headers: { Authorization: `Bearer ${token}` }
+    }).then(function () {
+        //suppression du travail dans le html  
+        // on filtre notre liste works en retirant celui qui a été supprimé sur le serveur
+        works = works.filter(function (work) {
+            return work.id !== id
+        })
+        // on réactualise l'affichage avec la nouvelle liste
+        Afficher_Liste(works);
+        Afficher_Liste_modal(works);
+
+    }).catch(
+        //affichage de l'erreur
+        (error) => {
+            console.log(error)
+
+        }
+    )
+};
+
+
+
+// Remplissage du menu déroulant des catégories pour l'ajout de projets dans la modale
 const categorieElement = form.querySelector("select");
 for (let i = 0; i < categories.length; i++) {
     let optionElement = document.createElement("option");
